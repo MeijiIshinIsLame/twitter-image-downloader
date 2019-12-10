7872c9a2cd853d071d7f39f6e8d6ccb0eac61f2b
meijiishinislame <meijiishinislame@gmail.com>
2019-12-09 23:15:53 -1000

    added notes, fixed formatting

--- twitter_backend.py
+++ twitter_backend.py
@@ -3,6 +3,7 @@ from tweepy import OAuthHandler
 import json
 
 
+#initializes api
 def initialize_tweepy(consumer_key, consumer_secret, access_token, access_secret):
  
     @classmethod
@@ -28,7 +29,7 @@ def initialize_tweepy(consumer_key, consumer_secret, access_token, access_secret
         print("Tweepy auth failed")
     
 
-
+#gets account list from txt file
 def get_accounts(filepath):
 
     name_list = []
@@ -42,10 +43,12 @@ def get_accounts(filepath):
     return name_list
 
 
+#gets tweet data
 def get_tweets(account_name, api):
 
     tweets = []
-    
+
+    #uses cursor to traverse through all tweets (until it hits the limit)
     for status in tweepy.Cursor(api.user_timeline, screen_name=account_name, tweet_mode="extended").items():
         tweets.append(status)
         print("tweets appended: ", len(tweets))
@@ -53,6 +56,7 @@ def get_tweets(account_name, api):
     return tweets
 
 
+#get image data from tweet, put it in a set so there are no duplicates
 def get_images(tweets):
 
     image_set = set()
@@ -64,20 +68,17 @@ def get_images(tweets):
             image_set.add(media[0]['media_url'])
 
     return image_set
-
-
     
 
 if __name__ == "__main__":
+    
     consumer_key = 'LWmcAOW8h8KW7k6bQ5uoDcjQc'
     consumer_secret = '8qkaVo4wGc9H7TYgduCGIO0par6cl7ti7fTvS6pqoGgz47Mj06'
     access_token = '1644845690-M3l9cOmqvVN0e0uNCNZkIHfYEOM76iYQWbimJwT'
     access_secret = 'pNhRXvZ1U2U0WAoQ29i9S5wuhH87jFrNC0BfuDqIPq0QE'
 
     api = initialize_tweepy(consumer_key, consumer_secret, access_token, access_secret)
-
     username_list = get_accounts("usernames.txt")
-
     tweets = []
 
     for name in username_list:
